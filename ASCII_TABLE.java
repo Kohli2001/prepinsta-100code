7910c0923ec1e82fdd84dda6c9b9e3d3f82a4619
kamleshkumarkohli <kamleshkumarkohli.cse2019@dscet.ac.in>
2022-11-02 01:03:52 +0530

    100 Code challenge

---
+++ ASCII_TABLE.java
@@ -0,0 +1,45 @@
+//1) code to find positive and negative number //
+// import java.util.*;
+import java.util.Scanner;
+public class ASCII_TABLE{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+
+        System.out.println("enter your number");
+        int num = sc.nextInt();
+
+        //TO FIND POSITIVE & NEGATIVE NUMBER.......
+
+//         if(num>0){
+//             System.out.println("the given number is postive");
+//         }
+//         else if(num<0){
+//             System.out.println("the given number is negative");
+//         }
+//         else{
+//             System.out.println("the number is not negative nor positive");
+//         }
+//     }
+
+
+// }
+
+        //TO FIND EVEN & ODD NUMBER.......
+
+        //  if(num%2==0){
+        //     System.out.println("the guven number is EVEN");
+        //  }
+        //  else if(num%2!=0){
+        //     System.out.println("the given number is ODD");
+        //  }
+        //  else{
+        //     System.out.println("the given number is either even nor odd");
+        //  }
+         
+        //TO FIND THE SUM OF FIRST N NATURAL NUMBER ......
+
+        }
+    }
+    
+}
