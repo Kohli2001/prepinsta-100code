bcbfab65322a2f33ab8374c8f4719f88a2ed6c01
kamlesh kumar kohli <93222536+Kohli2001@users.noreply.github.com>
2023-10-08 23:06:54 +0530

    Qno4

---
+++ Arrays/Qno4.java
@@ -0,0 +1,18 @@
+package codePreArrays;
+
+public class Qno4 {
+	
+	//Reverse an Array :
+	
+	public static void main(String[] args) {
+		int[] arr= {12,13,14,15,16};
+//		int[] arr1= new int[arr.length];
+		for(int i=arr.length-1; i>=0; i--) {
+			
+			System.out.println(arr[i]);
+		}
+		
+		
+	}
+
+}
